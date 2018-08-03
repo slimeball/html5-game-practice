647c80ad2a0e289f31710383df984a027cd75a0f
noah.g <noah.g@iv66.net>
2018-08-03 18:11:05 +0800

    update

--- phaser/static/js/practice.js
+++ phaser/static/js/practice.js
@@ -1,6 +1,35 @@
 let player;
+let stars;
+let bombs;
 let platforms;
 let cursors;
+let score = 0;
+let scoreText;
+
+let collectStar = function (player, star) {
+  star.disableBody(true, true)
+  score += 10;
+  scoreText.setText('score:' + score);
+  if (stars.countActive(true) === 0){
+    stars.children.iterate(function (child) {
+      child.enableBody(true, child.x, 0, true, true);
+    })
+
+    let x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+    let bomb = bombs.create(x, 16, 'bomb');
+    bomb.setBounce(1);
+    bomb.setCollideWorldBounds(true);
+    bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+    bomb.allowGravity = false;
+  }
+}
+
+let hitBomb = function (player, bomb) {
+  this.physics.pause();
+  player.setTint(0xff0000);
+  player.anims.play('turn');
+  gameOver = true;
+}
 
 let preload = function () {
   this.load.image('sky', '/static/assets/sky.png');
@@ -45,7 +74,25 @@ let create = function () {
       repeat: -1
   });
   cursors = this.input.keyboard.createCursorKeys();
+  stars = this.physics.add.group({
+    key: 'star',
+    repeat: 11,
+    setXY: { x: 12,y: 0, stepX: 70 }
+  })
+
+  stars.children.iterate(function (child) {
+    child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+  });
+  bombs = this.physics.add.group();
   this.physics.add.collider(player, platforms);
+  this.physics.add.collider(stars, platforms);
+  this.physics.add.collider(bombs, platforms);
+  this.physics.add.overlap(player, stars, collectStar, null, this);
+
+  
+  this.physics.add.collider(player, bombs, hitBomb, null, this);
+
+  scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000'});
 }
 
 let update = function () {
